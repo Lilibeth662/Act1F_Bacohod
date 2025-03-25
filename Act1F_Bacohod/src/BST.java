c0784d8cb3e13cd11e696164453ed79df802415b
Lilibeth662 <lilibeth.bacohod@bisu.edu.ph>
2025-03-25 20:27:05 +0800

    BST.java

--- Act1F_Bacohod/src/BST.java
+++ Act1F_Bacohod/src/BST.java
@@ -1,5 +1,89 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class BST {
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
     }
+
+    Node root;
+
+    BST() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+
+    void inOrder() {
+        inOrderRec(root);
+        System.out.println();
+    }
+
+    void inOrderRec(Node root) {
+        if (root != null) {
+            inOrderRec(root.left);
+            System.out.print(root.key + " ");
+            inOrderRec(root.right);
+        }
+    }
+
+    void preOrder() {
+        preOrderRec(root);
+        System.out.println();
+    }
+
+    void preOrderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preOrderRec(root.left);
+            preOrderRec(root.right);
+        }
+    }
+
+    void postOrder() {
+        postOrderRec(root);
+        System.out.println();
+    }
+
+    void postOrderRec(Node root) {
+        if (root != null) {
+            postOrderRec(root.left);
+            postOrderRec(root.right);
+            System.out.print(root.key + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        int[] values = {10, 5, 15, 3, 8, 20};
+        for (int value : values) {
+            tree.insert(value);
+        }
+
+        System.out.println("In-Order Traversal:");
+        tree.inOrder();
+
+        System.out.println("Pre-Order Traversal:");
+        tree.preOrder();
+
+        System.out.println("Post-Order Traversal:");
+        tree.postOrder();
+}
 }
